5ef87a43545614445bef5ef1d3a46764fe6c55e7
christien1 <christien1@wit.edu>
2024-03-26 12:53:01 -0400

    Added comment

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    // added so can do pull req.
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
